6177c690112dac02abe20946cfa8d9e84a71a6fa
DESKTOP-8FU98QS\HAN <gyulbbe@gmail.com>
2024-03-31 13:47:50 +0900

    @Autowired 제거, 수정

--- src/main/java/com/hg/controller/IndexController.java
+++ src/main/java/com/hg/controller/IndexController.java
@@ -2,20 +2,15 @@ package com.hg.controller;
 
 import java.util.HashMap;
 
-//import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.hg.service.BoardService;
 
 @Controller
 public class IndexController {
 
-
-	@Autowired BoardService bService;
 
 //	Logger logger = Logger.getLogger(MemberController.class);
 
